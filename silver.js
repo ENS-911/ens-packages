9faabf638d1e1822d18071588f97bae8da91ec8b
Kenneth Brook <kenneth.brook@hotmail.com>
2023-12-18 11:06:34 -0500

    link fix

--- silver.js
+++ silver.js
@@ -203,7 +203,7 @@ function countTrigger() {
   const script = document.createElement('script');
           
 
-  script.src = `https://ensloadout.911emergensee.com/ens-packages/components/cb0.js.js`;
+  script.src = `https://ensloadout.911emergensee.com/ens-packages/components/count-bars/cb0.js.js`;
           
 
   document.head.appendChild(script);
